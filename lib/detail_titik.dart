a8fcb834cdac3b56c80336608809ea1d0fefa281
akbarltf <127316214+akbarltf@users.noreply.github.com>
2024-04-20 12:05:22 +0700

    Add files via upload

--- lib/detail_titik.dart
+++ lib/detail_titik.dart
@@ -1,130 +1,191 @@
 import 'package:flutter/material.dart';
 import 'package:kdgaugeview/kdgaugeview.dart';
 import 'package:vertical_percent_indicator/vertical_percent_indicator.dart';
+import 'homepage.dart';
 
 class DetailTitik extends StatelessWidget {
-  final String title;
-
-  const DetailTitik({Key? key, required this.title}) : super(key: key);
+  const DetailTitik({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 7,
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(title),
-            Text(
-              'Tungkop, Darussalam',
-              style: TextStyle(fontSize: 12),
-            ),
-          ],
-        ),
-        // actions: [
-        //   Padding(
-        //     padding: const EdgeInsets.all(8.0),
-        //     child: Text(
-        //       '14.10',
-        //       style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-        //     ),
-        //   ),
-        // ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: 300,
-              height: 300,
-              padding: EdgeInsets.all(10),
-              child: KdGaugeView(
-                minSpeed: 0,
-                maxSpeed: 100, 
-                speed: 70, 
-                animate: true,
-                duration: Duration(seconds: 3),
-                alertSpeedArray: [20, 50, 80],
-                alertColorArray: [Colors.green, Colors.orange, Colors.red],
-                unitOfMeasurement: "m/s2",
+      body: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).padding.top,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                iconSize: 35,
               ),
-            ),
-            // const SizedBox(height: 10), // Add some spacing
-            VerticalBarIndicator(
-              percent: 0.67,
-              header: 'Ketinggian Air',
-              footer: '0,67 meter',
-              height: 200,
-              width: 350,
-              color: [
-                Colors.blue.withOpacity(0.5),
-                Colors.blue,
-              ],
-              circularRadius: 0,
-            ),
-            
-            const SizedBox(height: 20), // Add some spacing
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 20),
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(color: Colors.grey),
-              ),
-              child: Table(
-                border: TableBorder.symmetric(
-                  inside: BorderSide.none,
-                ),
+              Row(
                 children: [
-                  TableRow(
-                    children: [
-                      TableCell(
-                        child: Padding(
-                          padding: const EdgeInsets.all(1.0),
-                          child: Center(child: Text('Kecepatan Aliran', style: TextStyle(fontWeight: FontWeight.bold),)),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 150),
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          'assets/Lokasi.png',
+                          width: 50,
+                          height: 50,
                         ),
-                      ),
-                      TableCell(
-                        child: Padding(
-                          padding: const EdgeInsets.all(1.0),
-                          child: Center(child: Text('70 m/s2')),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(height: 25),
+                            Text(
+                              'Titik A',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              'Tungkop, Darussalam',
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                  TableRow(
-                    children: [
-                      TableCell(
-                        child: Padding(
-                          padding: const EdgeInsets.all(1.0),
-                          child: Center(child: Text('Ketinggian Air', style: TextStyle(fontWeight: FontWeight.bold),)),
-                        ),
-                      ),
-                      TableCell(
-                        child: Padding(
-                          padding: const EdgeInsets.all(1.0),
-                          child: Center(child: Text('0,67 m')),
-                        ),
-                      ),
-                    ],
+                ],
+              ),
+            ],
+          ),
+          Container(
+            height: 3,
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+            color: const Color.fromARGB(255, 124, 124, 124),
+          ),
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 280,
+                    height: 280,
+                    padding: EdgeInsets.all(15),
+                    child: KdGaugeView(
+                      minSpeed: 0,
+                      maxSpeed: 100,
+                      speed: 70,
+                      animate: true,
+                      duration: Duration(seconds: 3),
+                      alertSpeedArray: [20, 50, 80],
+                      alertColorArray: [
+                        Colors.green,
+                        Colors.orange,
+                        Colors.red
+                      ],
+                      unitOfMeasurement: "m/s2",
+                    ),
                   ),
-                  
+                  VerticalBarIndicator(
+                    percent: 0.60,
+                    header: 'Ketinggian Air',
+                    footer: '0,60 meter',
+                    height: 150,
+                    width: 250,
+                    color: [
+                      Colors.blue.withOpacity(0.5),
+                      Colors.blue,
+                    ],
+                    circularRadius: 0,
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 20),
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.grey),
+                    ),
+                    child: Table(
+                      border: TableBorder.symmetric(
+                        inside: BorderSide.none,
+                      ),
+                      children: [
+                        TableRow(
+                          children: [
+                            TableCell(
+                              child: Padding(
+                                padding: const EdgeInsets.all(1.0),
+                                child: Center(
+                                    child: Text(
+                                  'Kecepatan Aliran',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                )),
+                              ),
+                            ),
+                            TableCell(
+                              child: Padding(
+                                padding: const EdgeInsets.all(1.0),
+                                child: Center(child: Text('70 m/s2')),
+                              ),
+                            ),
+                          ],
+                        ),
+                        TableRow(
+                          children: [
+                            TableCell(
+                              child: Padding(
+                                padding: const EdgeInsets.all(1.0),
+                                child: Center(
+                                    child: Text(
+                                  'Ketinggian Air',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                )),
+                              ),
+                            ),
+                            TableCell(
+                              child: Padding(
+                                padding: const EdgeInsets.all(1.0),
+                                child: Center(child: Text('0,60 m')),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
-            
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        tooltip: 'Back to Main',
-        child: Icon(Icons.place),
+      floatingActionButton: Container(
+        width: 75,
+        height: 75,
+        child: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const HomePage()),
+            );
+          },
+          backgroundColor: Colors.white,
+          child: Container(
+            width: 60.0,
+            height: 60.0,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/lokasiikon.png'),
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+        ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,8 @@
+// Import library yang dibutuhkan
 import 'package:flutter/material.dart';
 import 'halamanaliran.dart'; // Import halaman selanjutnya
+import 'detail_titik.dart';
+import 'detail_titik2.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,6 +13,9 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   TextEditingController _searchController = TextEditingController();
+  double topMarkerPosition = 50.0; // Posisi vertikal penanda lokasi atas
+  double bottomMarkerPosition = 150.0; // Posisi vertikal penanda lokasi bawah
+  double leftMarkerPosition = 100.0; // Posisi horizontal penanda lokasi
 
   @override
   Widget build(BuildContext context) {
@@ -82,57 +88,149 @@ class _HomePageState extends State<HomePage> {
                     width: 1.0,
                   ),
                 ),
-                child: Icon(Icons.add, color: Colors.black),
+                child: Image.asset('assets/Homepage.png'),
               ),
             ),
           ),
+          // Gambar penanda lokasi atas (Titik A)
+          Positioned(
+            top: topMarkerPosition + 120,
+            left: leftMarkerPosition + 160,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => DetailTitik()),
+                );
+              },
+              child: Row(
+                children: [
+                  Image.asset(
+                    'assets/Lokasi.png',
+                    width: 40.0, // Ubah lebar gambar ikon lokasi
+                    height: 50.0, // Ubah tinggi gambar ikon lokasi
+                  ),
+                  SizedBox(width: 10.0), // Spasi antara ikon dan lingkaran
+                  Container(
+                    width: 15.0, // Lebar lingkaran kecil
+                    height: 15.0, // Tinggi lingkaran kecil
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(
+                          255, 255, 17, 0), // Warna lingkaran kecil
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                  SizedBox(width: 5.0), // Spasi antara lingkaran dan teks
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '70 m/s2', // Informasi kecepatan
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        '0.67 m', // Informasi ketinggian air
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+// Gambar penanda lokasi bawah (Titik B)
+          Positioned(
+            bottom: bottomMarkerPosition + 25,
+            left: leftMarkerPosition + 169, // Menggeser posisi untuk titik B
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => DetailTitik2()),
+                );
+              },
+              child: Row(
+                children: [
+                  Image.asset(
+                    'assets/Lokasi.png',
+                    width: 40.0, // Ubah lebar gambar ikon lokasi
+                    height: 50.0, // Ubah tinggi gambar ikon lokasi
+                  ),
+                  SizedBox(width: 10.0), // Spasi antara ikon dan lingkaran
+                  Container(
+                    width: 15.0, // Lebar lingkaran kecil
+                    height: 15.0, // Tinggi lingkaran kecil
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(
+                          255, 13, 255, 0), // Warna lingkaran kecil
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                  SizedBox(width: 5.0), // Spasi antara lingkaran dan teks
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '50 m/s2', // Informasi kecepatan
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        '0.50 m', // Informasi ketinggian air
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(width: 5.0), // Spasi antara teks dan ikon
+                ],
+              ),
+            ),
+          ),
+
           // Lingkaran kecil "tinggi"
           Positioned(
             bottom: 100.0,
-            left: 20.0,
-            child: Column(
+            left: 37.0,
+            child: Row(
               children: [
                 Container(
                   width: 20.0,
                   height: 20.0,
                   decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 0, 249, 9),
+                    color: Color.fromARGB(255, 255, 0, 0),
                     shape: BoxShape.circle,
                   ),
                 ),
-                SizedBox(height: 5.0),
+                SizedBox(width: 5.0),
+                Text('Tinggi', style: TextStyle(color: Colors.white)),
               ],
             ),
           ),
-          // Tulisan "Tinggi" di sebelah lingkaran "tinggi"
+          // Lingkaran kecil "Normal"
           Positioned(
-            bottom: 100.0,
-            left: 50.0,
-            child: Text('Tinggi', style: TextStyle(color: Colors.white)),
-          ),
-          // Lingkaran kecil "rendah"
-          Positioned(
-            bottom: 70.0,
-            left: 20.0,
-            child: Column(
+            bottom: 60.0,
+            left: 37.0,
+            child: Row(
               children: [
                 Container(
                   width: 20.0,
                   height: 20.0,
                   decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 247, 16, 0),
+                    color: Color.fromARGB(255, 0, 255, 64),
                     shape: BoxShape.circle,
                   ),
                 ),
-                SizedBox(height: 5.0),
+                SizedBox(width: 5.0),
+                Text('Normal', style: TextStyle(color: Colors.white)),
               ],
             ),
-          ),
-          // Tulisan "Rendah" di sebelah lingkaran "rendah"
-          Positioned(
-            bottom: 70.0,
-            left: 50.0,
-            child: Text('Rendah', style: TextStyle(color: Colors.white)),
           ),
         ],
       ),
